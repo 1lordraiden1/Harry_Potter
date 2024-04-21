3e2cd4f276a97e9528a202f876b6349bf0e2da85
LordRaiden <zeyadmo810@gmail.com>
2024-04-21 20:41:19 +0300

    delete init screen

---
+++ lib/api_handler.dart
@@ -0,0 +1,22 @@
+
+import 'package:dio/dio.dart';
+import 'package:users_app/character_model.dart';
+
+class DataHandler {
+
+  static String apiKey = "https://hp-api.onrender.com/api";
+
+  static var  dio = Dio();
+
+
+  static Future<List<Character>> getCharacters() async {
+    var response = await dio.get('$apiKey/characters');
+
+    if (response.statusCode == 200){
+      final responseData = response.data as List<dynamic>;
+      final characters = responseData.map((character) => Character.fromJson(character)).toList();
+      return characters;
+    }
+    return [];
+  }
+}
